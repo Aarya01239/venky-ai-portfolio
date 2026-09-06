086531fd58aa6569a1ed2bcdcb3f2408ed38e76a
Aarya01239 <160004032+Aarya01239@users.noreply.github.com>
2026-09-07 01:24:56 +0530

    Update PortraitCard.tsx

--- src/components/PortraitCard.tsx
+++ src/components/PortraitCard.tsx
@@ -79,7 +79,7 @@ export const PortraitCard: React.FC<PortraitCardProps> = ({ variant = 'hero', cl
           {customPhoto ? (
             <img
               id={`portrait-img-${variant}`}
-              src="/venky-profile.jpg"
+           src="/WhatsApp Image 2026-09-06 at 23.05.35.jpeg"
               alt="Venkata Kumar Pulapa"
               className="w-full h-full object-cover object-top transition duration-500 group-hover:scale-[1.02]"
             />
